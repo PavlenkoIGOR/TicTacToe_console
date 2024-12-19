73751cbdb857d7cd22439c135e126110898c3fce
iLLusio <illusiopard@gmail.com>
2024-12-19 19:02:45 +0300

    drawX drawO

--- TicTacToe_console/Program.cs
+++ TicTacToe_console/Program.cs
@@ -5,6 +5,57 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            DrawField(31, 30, 10);
+            DrawX(31, 30, 10);
+            DrawO(31, 30, 10);
+        }
+        static void DrawField(int w, int h, int cellSize)
+        {
+            for (int y = 0; y <= h; y += cellSize)
+            {
+                for (int x = 0; x < w; x++)
+                {
+                    Console.SetCursorPosition(x, y);
+                    Console.WriteLine("█");
+                }
+            }
+            for (int x = 0; x <= w; x += cellSize)
+            {
+                for (int y = 0; y <= h; y++)
+                {
+                    Console.SetCursorPosition(x, y);
+                    Console.WriteLine("█");
+                }
+            }
+        }
+
+        static void DrawX(int x, int y, int size)
+        {
+            for (int i = x; i <= x + size; i++)
+            {
+                for (int j = y; j <= y + size; j++)
+                {
+                    if (i - j == x - y || i + j == y + x + size)
+                    {
+                        Console.SetCursorPosition(i, j);
+                        Console.WriteLine("█");
+                    }
+                }
+            }
+        }
+        static void DrawO(int x, int y, int size)
+        {
+            for (int i = x; i <= x + size; i++)
+            {
+                for (int j = y; j <= y + size; j++)
+                {
+                    if (i == x || i == x + size || j == y || j == y + size)
+                    {
+                        Console.SetCursorPosition(i, j);
+                        Console.WriteLine("█");
+                    }
+                }
+            }
         }
     }
 }
